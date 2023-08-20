8033c7d2bf99ea18703baa5b5315e1e8b3a6c442
chetanmanoj <chetanmanoj2001@gmail.com>
2023-08-21 00:09:03 +0530

    Built user model and register view

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from . import views
+# from . import views
+from .views import Register
 
 urlpatterns = [
-    path('users/', views.users, name='users'),
+    path('register', Register.as_view())
 ]
--- users/views.py
+++ users/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
-
-from django.http import HttpResponse
-
-def users(request):
-    return HttpResponse("Hello world!")
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import UserSerializer
+ 
+class Register(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data = request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
