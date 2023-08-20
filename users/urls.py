3930ad4ef24042a1dbdc3d7cbc7e078d47f7ddc5
chetanmanoj <chetanmanoj2001@gmail.com>
2023-08-21 00:28:16 +0530

    Built loign view

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 # from . import views
-from .views import Register
+from .views import Register, Login
 
 urlpatterns = [
-    path('register', Register.as_view())
+    path('register', Register.as_view()),
+    path('login', Login.as_view())
 ]
--- users/views.py
+++ users/views.py
@@ -1,10 +1,31 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.exceptions import AuthenticationFailed
+from .models import User
 from .serializers import UserSerializer
  
+#  register user
 class Register(APIView):
     def post(self, request):
         serializer = UserSerializer(data = request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
+    
+# login user 
+class Login(APIView):
+    def post(self,request):
+        email = request.data['email']
+        password = request.data['password']
+
+        user = User.objects.filter(email = email).first()
+
+        if user is None:
+            raise AuthenticationFailed('User not found')
+        
+        if not user.check_password(password):
+            raise AuthenticationFailed('Password incorrect!')
+        
+        return Response({
+            'message':'nice'
+        })
